d0b1c2431fc29d45f185ad0af1b39adae1d570c2
gytrhu6u-g-2 <gytrhu6u@icloud.com>
2023-06-01 23:51:35 +0900

    DBに登録する処理の追加

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Blog;
+use App\Http\Requests\BlogRequest;
+use Illuminate\Support\Facades\DB;
 
 use function PHPUnit\Framework\isNull;
 
@@ -11,7 +13,6 @@ class BlogController extends Controller
 {
     /**
      * ブログ一覧を表示する
-     * 
      * @return view
      */
     public function showList()
@@ -32,9 +33,41 @@ class BlogController extends Controller
 
        if (is_null($blog)) {
         session()->flash('err_msg','データがありません。');
-        return redirect('/');
+        return redirect(route('blogs'));
        }
        
        return view('blog.detail', ['blog' => $blog]);
+    }
+
+    /**
+     * ブログ画面を表示する
+     * @return view
+     */
+    public function showCreate()
+    {
+        return view('blog.form');
+    }
+
+    /**
+     * ブログを登録する
+     * @return view
+     */
+    public function exeStore(BlogRequest $request)
+    {
+        // ブログのデータを受け取る
+        $input = $request->all();
+
+        DB::beginTransaction();
+        try {
+            // ブログを登録
+            Blog::create($input);
+            DB::commit();
+        } catch(\Throwable $e) {
+            abort(500);
+            DB::rollBack();
+        }
+
+        session()->flash('err_msg','ブログを登録しました');
+        return redirect(route('blogs'));
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 // ブログ一覧画面を表示
-Route::get('/', 'App\Http\Controllers\BlogController@showList')->name('blog.blogs');
+Route::get('/', 'App\Http\Controllers\BlogController@showList')->name('blogs');
+
+// ブログ画面の表示
+Route::get('/blog/create', 'App\Http\Controllers\BlogController@showCreate')->name('create');
+
+// ブログ登録
+Route::post('/blog/store', 'App\Http\Controllers\BlogController@exeStore')->name('store');
 
 // ブログの詳細画面を表示
 Route::get('/blog/{id}', 'App\Http\Controllers\BlogController@showDetail')->name('show');
